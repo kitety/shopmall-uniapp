60365943645c3be43bd12647e600a8d990519140
kitety <aizaizuori@foxmail.com>
2022-06-05 13:02:11 +0800

    feat: init

---
+++ pages/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <view class="index">
+        <view class="f-active-color">
+            232323
+        </view>
+        <view class="iconfont icon-lajitong">
+
+        </view>
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: 'Hello'
+            }
+        },
+        onLoad() {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style>
+
+</style>
